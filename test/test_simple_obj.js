d3f747d37493fe7b6faea91dca0d242b5a33db51
Hamish Hutchings <moredhel@aoeu.me>
2016-10-12 09:05:59 +0100

    Expand tests to work with new data structure

--- test/test_simple_obj.js
+++ test/test_simple_obj.js
@@ -1,12 +1,12 @@
 var chai = require('chai');
 var expect = chai.expect;
 
+var Immutable = require('immutable');
 var Coverage = require('../build/coverage.js');
 var Infer = require('../build/infer.js');
 var obj = Coverage.wrap(require('../data/simple_obj.js'), 'simple_obj');
 
-// disable printing of outputs
-// Coverage.outputTypes(false);
+Coverage.outputTypes(false);
 
 describe('Base', function() {
 
@@ -24,48 +24,54 @@ describe('Base', function() {
   it('no duplication', function() {
     obj.a = 2;
     obj.a = 2;
-    var types = Infer.types(Coverage.object_map, 'simple_obj').get('a');
-    expect(types).to.eq('integer');
+    var types = Infer.types(Coverage.object_map, 'simple_obj').get('a').get('type').toJS();
+    expect(types).deep.to.eq(['integer']);
   });
 
   it('union types (sorted)', function() {
     obj.c = 2;
     obj.c = 'hello';
-    var types = Infer.types(Coverage.object_map, 'simple_obj').get('c');
-    expect(types).to.eq('integer|string');
+    var types = Infer.types(Coverage.object_map, 'simple_obj').get('c').get('type').toJS();
+    expect(types).deep.to.eq(['integer', 'string']);
   });
 
 
   it('union base/object', function() {
     obj.d = true;
     obj.d = {};
+    obj.d.e = "hello";
 
     expect(function() {
-      Infer.types(Coverage.object_map, 'simple_obj').get('c');
-    }).to.throw(Error);
+      var x = Infer.types(Coverage.object_map, 'simple_obj').get('d');
+    }).to.not.throw(Error);
   });
 
+  it('union base/object no access', function() {
+    obj.d = true;
+    obj.d = {};
+    obj.d = 'hello';
+    obj.d = Immutable.Map({});
+
+    var x;
+    expect(function() {
+      x = Infer.types(Coverage.object_map, 'simple_obj').get('d');
+    }).to.not.throw(Error);
+
+    expect(x.get('object0').toJS())
+      .deep.to.eq({});
+
+  });
+
+  it('preserve keys for object types', function() {
+    obj.d = Immutable.Map({});
+    var size = obj.d.size;
+
+    var x = Infer.types(Coverage.object_map, 'simple_obj').get('d');
+    expect(x.get('object0').toJS())
+      .deep.to.eq({
+        size: {
+          type: ['integer']
+        }
+      });
+  });
 });
-
-describe('Destructive', function() {
-
-  beforeEach(function() {
-    Coverage.RESET();
-  });
-
-  it('union base/object', function() {
-    obj.d = true;
-    obj.d = {};
-
-    expect(function() {
-      Infer.types(Coverage.object_map, 'simple_obj').get('c');
-    }).to.throw(Error);
-  });
-
-  it('union types (sorted)', function() {
-    obj.c = 2;
-    obj.c = 'hello';
-    var types = Infer.types(Coverage.object_map, 'simple_obj').get('c');
-    expect(types).to.eq('integer|string');
-  });
-})
